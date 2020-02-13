fe9bd639d7076434e5739e37049fcbf5dfd2c753
doryski <dominatorskyyy@gmail.com>
2020-02-13 19:00:00 +0100

    changed file titles and increased canvas

--- vars.js
+++ vars.js
@@ -1,10 +1,10 @@
 var canvasParams = {
-    x: 600,
+    x: 1000,
     y: 550
 }
 
 let boids = [];
-var numOfBoids = 10;
+var numOfBoids = 20;
 
 let foods = [];
-var numOfFood = 1;
+var numOfFood = 2;
